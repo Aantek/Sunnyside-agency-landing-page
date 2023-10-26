657a76531023b1ccbda6c119fce37ecd7e848164
Antuan-1001 <75267997+Antuan-1001@users.noreply.github.com>
2023-10-26 16:22:55 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        fraunces : ['Fraunces', 'serif'],
+        barlow : ['Barlow', 'sans']
+      }
+    },
+  },
+  plugins: [],
+}
+
